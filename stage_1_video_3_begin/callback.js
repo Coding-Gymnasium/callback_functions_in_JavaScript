c4757e80834a7772af790b81747048a019ce45ac
Nico Rithner <nico@altvia.com>
2021-07-18 18:54:58 -0600

    Add anonymous functions

--- stage_1_video_3_begin/callback.js
+++ stage_1_video_3_begin/callback.js
@@ -1,9 +1,18 @@
-function sayHello() {
-    console.log('Hello');
-}
+// function sayHello() { // as a function declaration
+//const sayHello = function () { // as a function expression
+//    console.log('Hello');
+//}
 
 function executeCallback(callback) {
   callback();
 }
 
-executeCallback(sayHello);
+// Anonymous function
+executeCallback(function() { // inlining. Writing a function in line
+  console.log('Hello');
+});
+
+// Print Goodbye
+executeCallback(function() {
+  console.log('Goodbye');
+});
