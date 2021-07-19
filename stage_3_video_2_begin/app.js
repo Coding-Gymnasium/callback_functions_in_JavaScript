1edb4ad4aeb42f952f4f0c1a417f2b86342de14f
Nico Rithner <nico@altvia.com>
2021-07-19 08:58:18 -0600

    Using callbacks on text inputs
    Co-authored-by: Nico nicorithner@gmail.com

--- stage_3_video_2_begin/app.js
+++ stage_3_video_2_begin/app.js
@@ -1,10 +1,20 @@
 const nameInput = document.getElementById('name');
 const messageTextArea = document.getElementById('message');
 
+//nameInput.addEventListener('click', e => e.target.className = 'highlight');
+
 nameInput.addEventListener('focus', event => {
   event.target.className = 'highlight';
 });
 
 nameInput.addEventListener('blur', event => {
+  event.target.className = '';
+});
+
+messageTextArea.addEventListener('focus', event => {
+  event.target.className = 'highlight';
+});
+
+messageTextArea.addEventListener('blur', event => {
   event.target.className = '';
 });
