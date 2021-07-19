b2f3fce7166cd96467310e71f27c97346962e23c
Nico Rithner <nico@altvia.com>
2021-07-19 08:44:17 -0600

    Make div1 flash using setInterval
    Co-authored-by: Nico nicorithner@gmail.com

--- stage_3_video_1_begin/app.js
+++ stage_3_video_1_begin/app.js
@@ -15,10 +15,27 @@ function makeGreen(element) {
     element.style.backgroundColor = "green";
 }
 
+function makeWhite(element) {
+    element.style.backgroundColor = "white";
+}
+
 function addStyleToElement(element, callback) {
     callback(element);
 }
 
-addStyleToElement(div1, makeRed);
+//addStyleToElement(div1, makeRed);
 addStyleToElement(div2, makeBlue);
 addStyleToElement(div3, makeGreen);
+
+function switchColors(element) {
+    setInterval(function () {
+        if (element.style.backgroundColor === "red")
+            makeWhite(element);
+        else if (element.style.backgroundColor === "white")
+            makeBlue(element);
+        else
+            makeRed(element);
+    }, 1000);
+}
+
+switchColors(div1);
