e361dac4a66ee660474e94b29e0ca86dd2567be8
Nico Rithner <nico@altvia.com>
2021-07-19 08:13:41 -0600

    Using a Repeat Timer with setInterval
    Co-authored-by: Nico nicorithner@gmail.com

--- stage_2_video_3_begin/clock.js
+++ stage_2_video_3_begin/clock.js
@@ -21,3 +21,10 @@ function getTime() {
 function tickClock() {
   clockSection.textContent = getTime();
 }
+
+tickClock(); // call it manually first so it loads inmmediately.
+setInterval(tickClock, 1000);
+
+// With an anonymous function
+// setInterval(() => clockSection.textContent = getTime(), 1000);
+// an anonymous function is not ideal in this case because we can manually make the clock load inmediately. The 'clock goes here' sign shows first before the clock. In this case the arrow function works better.
