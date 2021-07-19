e834ebeba29c869a296c096c78f1572afdb98d97
Nico Rithner <nico@altvia.com>
2021-07-19 07:59:11 -0600

    Add timer and use an anonymous function
    Co-authored-by: Nico nicorithner@gmail.com

--- stage_2_video_1_begin/surprise.js
+++ stage_2_video_1_begin/surprise.js
@@ -1,5 +1,18 @@
 const surpriseSection = document.getElementById('surprise');
 
-function showSurprise() {
-    surpriseSection.textContent = '🎉 Surprise! 🎉';
-}
+//function showSurprise() {
+//    surpriseSection.textContent = '🎉 Surprise! 🎉';
+//}
+
+const randomTime = Math.random() * 4000;
+
+//setTimeout(showSurprise,  randomTime);
+
+//// with an anonymous function
+
+//setTimeout(() => {
+//  surpriseSection.textContent = '🎉 Surprise! 🎉';
+//}, randomTime);
+
+// As a one liner
+setTimeout(() => surpriseSection.textContent = '🎉 Surprise! 🎉', randomTime);
